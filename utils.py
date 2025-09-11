75eb669cbe46a21487e3ab2eacc381489eb59a8c
aradhyasharma894-tech <aradhyasharma894@gmail.com>
2025-09-11 13:25:20 +0530

    Added helper functions in utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+# utils.py
+import numpy as np
+
+def normalize_image(img):
+    """Normalize image pixels to 0-1"""
+    return img / 255.0
+
+def dummy_function():
+    print("This is a placeholder helper function")
