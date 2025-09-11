00222fb1b1870cd0aecdc4a9e28fca09f1d4cfd2
aradhyasharma894-tech <aradhyasharma894@gmail.com>
2025-09-11 13:27:56 +0530

    Added visualization.py stub for plotting results

---
+++ visualization.py
@@ -0,0 +1,11 @@
+# visualization.py
+import matplotlib.pyplot as plt
+
+def plot_dummy_results():
+    """Dummy function to simulate plotting"""
+    plt.plot([0,1,2],[0,1,0])
+    plt.title("Dummy Results")
+    plt.show()
+
+if __name__ == "__main__":
+    plot_dummy_results()
